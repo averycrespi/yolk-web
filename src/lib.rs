f3aea8286df332fb59b01c86c27b64986a6091eb
Avery Crespi <averycrespi@gmail.com>
2019-08-16 20:42:56 -0700

    Implment basic functionality

---
+++ src/lib.rs
@@ -0,0 +1,71 @@
+use yew::{html, Component, ComponentLink, Html, Renderable, ShouldRender};
+use yolk::ast::YololNode;
+use yolk::{optimize, parse, transpile};
+
+pub struct Model {
+    output: String,
+}
+
+pub enum Msg {
+    Input(String),
+}
+
+impl Model {
+    fn new() -> Self {
+        Model {
+            output: String::new(),
+        }
+    }
+
+    fn transpile(&mut self, input: String) {
+        let yolk = match parse(&input) {
+            Ok(yolk) => yolk,
+            Err(err) => {
+                self.output = err.to_string();
+                return;
+            }
+        };
+        let (yolol, context) = match transpile(&yolk) {
+            Ok((yolol, context)) => (yolol, context),
+            Err(err) => {
+                self.output = err.to_string();
+                return;
+            }
+        };
+        let optimized = optimize(&yolol, &context);
+        self.output = YololNode::format_as_program(&optimized);
+    }
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Model::new()
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Input(s) => {
+                self.transpile(s);
+                true
+            }
+        }
+    }
+}
+
+impl Renderable<Model> for Model {
+    fn view(&self) -> Html<Self> {
+        html! {
+            <div class="yolk-web">
+                <div>
+                    <textarea class=("text",) cols="80" rows="20" oninput=|e| Msg::Input(e.value)/>
+                </div>
+                <div>
+                    <textarea class=("text",) cols="80" rows="20" readonly="readonly" value={self.output.to_string()}/>
+                </div>
+            </div>
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use yew::prelude::App;
+
+use yolk_web::Model;
+
 fn main() {
-    println!("Hello, world!");
+    yew::initialize();
+    let app: App<Model> = App::new();
+    app.mount_to_body();
+    yew::run_loop();
 }
